785df918232f91cc029998684a8b1eacc4746b9d
hariharanr <hramesh@symbiance.com>
2024-12-24 16:20:00 +0530

    "running method added"
    (cherry picked from commit 2ce67ee66bbcc86e20114aebe2d1a6a4a36ab2d1)

--- src/main/java/com/java/cherry_pic/sample/service/Cat.java
+++ src/main/java/com/java/cherry_pic/sample/service/Cat.java
@@ -3,6 +3,12 @@ package com.java.cherry_pic.sample.service;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Cat.......");
+
+        new Cat().eating();
+    }
+
+    public void eating(){
+        System.out.println("cat is eating....");
         new Cat().running();
     }
     public void running(){
