2ce67ee66bbcc86e20114aebe2d1a6a4a36ab2d1
hariharanr <hramesh@symbiance.com>
2024-12-24 16:20:00 +0530

    "running method added"

--- src/main/java/com/java/cherry_pic/sample/service/Cat.java
+++ src/main/java/com/java/cherry_pic/sample/service/Cat.java
@@ -3,5 +3,9 @@ package com.java.cherry_pic.sample.service;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Cat.......");
+        new Cat().running();
+    }
+    public void running(){
+        System.out.println("cat is running...!!");
     }
 }
